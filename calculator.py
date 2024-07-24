e792eaf8a783a330bd682a8fa629f7370541f7ac
Opor123 <you@example.com>
2024-07-24 17:24:44 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -5,4 +5,11 @@ def substract(a,b):
 def multiply(a,b):
     return a*b
 def divide(a,b):
-    return a/b
+    if a == 0 or b==0:
+        print("Division is error")
+    else:
+        return a/b
+print(f"Answer of Add = {add(4,6)}")
+print(f"Answer of Minus = {substract(7,18)}")
+print(f"Answer of Multiplied = {multiply(4,6)}")
+print(f"Answer of Divide = {divide(4,6)}")
