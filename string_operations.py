093f5f711f5854a9137916d0778b77a13a95c14e
Opor123 <you@example.com>
2024-07-24 17:43:22 +0700

    Update string_operations.py

--- string_operations.py
+++ string_operations.py
@@ -1,10 +1,12 @@
 def reverse_string(a):
+    a=""
     return a[::-1]
-
-sample_string="hello world"
-up_string="HELLO WORLD"
-print("Original: ", sample_string)
-print(f"Reversed: {reverse_string(sample_string)}")
-print("Capitalize: ", sample_string.capitalize())
-print("Lowercase: ",up_string.lower())
-print("Uppercase: ", sample_string.upper())
+def capitalize_string(a):
+    a=""
+    return a.capitalize()
+def Lowercase_string(a):
+    a=""
+    return a.lower()
+def Uppercase_stiring(a):
+    a=""
+    return a.upper()
