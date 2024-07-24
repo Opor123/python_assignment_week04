94bd9ed8aa327cc7588b0b3cdf9eaba89b0bd9cb
Opor123 <you@example.com>
2024-07-24 17:36:21 +0700

    Update string_operations.py

---
+++ string_operations.py
@@ -0,0 +1,10 @@
+def reverse_string(a):
+    return a[::-1]
+
+sample_string="hello world"
+up_string="HELLO WORLD"
+print("Original: ", sample_string)
+print(f"Reversed: {reverse_string(sample_string)}")
+print("Capitalize: ", sample_string.capitalize())
+print("Lowercase: ",up_string.lower())
+print("Uppercase: ", sample_string.upper())
