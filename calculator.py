63615157317c46b50196baa4957e67648dac0224
Opor123 <you@example.com>
2024-07-24 17:43:25 +0700

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -9,7 +9,3 @@ def divide(a,b):
         print("Division is error")
     else:
         return a/b
-print(f"Answer of Add = {add(4,6)}")
-print(f"Answer of Minus = {substract(7,18)}")
-print(f"Answer of Multiplied = {multiply(4,6)}")
-print(f"Answer of Divide = {divide(4,6)}")
