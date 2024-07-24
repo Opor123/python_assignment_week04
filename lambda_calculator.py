0dff26104a8b3203471ecdbee9392917e902453d
Opor123 <you@example.com>
2024-07-24 17:43:28 +0700

    Update lambda_calculator.py

--- lambda_calculator.py
+++ lambda_calculator.py
@@ -2,7 +2,3 @@ add=lambda a,b:a+b
 sub=lambda a,b:a-b
 multi=lambda a,b:a*b
 divide = lambda a,b:a/b if b !=0 else "Division is error"
-print(f"Add = {add(5,4)}")
-print(f"substract = {sub(5,4)}")
-print(f"Multipiled = {multi(5,4)}")
-print(f"Divide = {divide(5,4)}")
