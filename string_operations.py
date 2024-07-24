b24431210c7e859ae5fac9c6a20b401c275c23ea
Opor123 <you@example.com>
2024-07-24 17:53:25 +0700

    fix

--- string_operations.py
+++ string_operations.py
@@ -1,12 +1,14 @@
 def reverse_string(a):
-    a=""
+    
     return a[::-1]
 def capitalize_string(a):
-    a=""
+    
     return a.capitalize()
 def Lowercase_string(a):
-    a=""
+    
     return a.lower()
 def Uppercase_stiring(a):
-    a=""
+    
     return a.upper()
+
+print(reverse_string("Hello world"))
