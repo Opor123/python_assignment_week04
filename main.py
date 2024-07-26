419b3bd890b772757a7c624d35a7222d5c52141e
Opor123 <you@example.com>
2024-07-26 10:39:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,5 @@ import utilities as utilities
 print("Add : ", utilities.cal.add(5,4))
 print("Reversed : ", utilities.string.reverse_string("Hello world"))
 print("Minus: ",utilities.lam.sub(4,5))
+
+print("Hello World")
